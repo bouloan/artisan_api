6a1b523b50dae35025117a41e4c98c3bc7fe2a9d
Anthony <boulord.anthony@gmail.com>
2020-08-16 15:38:13 +0200

    [bugfix] modifications to allow deployment on heroku

--- src/env.ts
+++ src/env.ts
@@ -1,28 +1,28 @@
 import dotenv from 'dotenv';
-// don't try to load .env file on Heroku
-if (process.env.NODE_ENV !== 'production') {
-	dotenv.config();
-}
+// // don't try to load .env file on Heroku
+// if (process.env.NODE_ENV !== 'production') {
+dotenv.config();
+// }
 
-let env = process.env.NODE_ENV!.toUpperCase();
+// let env = process.env.NODE_ENV!.toUpperCase();
 
 //port
-export const PORT = process.env[`PORT_${env}`];
+export const PORT = process.env[`PORT`];
 //MongoDb
-export const DB_URL = process.env[`DB_URL_${env}`];
+export const DB_URL = process.env[`DB_URL`];
 
 //JWT
 
-export const JWT_TOKEN_SECRET_KEY = process.env[`TOKEN_SECRET_KEY_${env}`],
-	JWT_REFRESHTOKEN_SECRET_KEY = process.env[`REFRESH_TOKEN_SECRET_KEY_${env}`],
-	TOKEN_EXPIRATION = process.env[`TOKEN_EXPIRATION_${env}`],
-	REFRESH_TOKEN_EXPIRATION = process.env[`REFRESH_TOKEN_EXPIRATION_${env}`];
+export const JWT_TOKEN_SECRET_KEY = process.env[`TOKEN_SECRET_KEY`],
+	JWT_REFRESHTOKEN_SECRET_KEY = process.env[`REFRESH_TOKEN_SECRET_KEY`],
+	TOKEN_EXPIRATION = process.env[`TOKEN_EXPIRATION`],
+	REFRESH_TOKEN_EXPIRATION = process.env[`REFRESH_TOKEN_EXPIRATION`];
 
 //MAILJET
-export const MAILJET_API_KEY_PUBLIC = process.env[`MJ_APIKEY_PUBLIC_${env}`],
-	MAILJET_API_KEY_PRIVATE = process.env[`MJ_APIKEY_PRIVATE_${env}`];
+export const MAILJET_API_KEY_PUBLIC = process.env[`MJ_APIKEY_PUBLIC`],
+	MAILJET_API_KEY_PRIVATE = process.env[`MJ_APIKEY_PRIVATE`];
 
 //MAIL
-export const MAIL_SENDER = process.env[`MAIL_SENDER_${env}`],
-	MAIL_WRITER = process.env[`MAIL_WRITER_${env}`],
-	MAIL_SUBJECT = process.env[`MAIL_SUBJECT_${env}`];
+export const MAIL_SENDER = process.env[`MAIL_SENDER`],
+	MAIL_WRITER = process.env[`MAIL_WRITER`],
+	MAIL_SUBJECT = process.env[`MAIL_SUBJECT`];
